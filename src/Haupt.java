754e90a6efa3273f239a869f1c4f680f5448a2bd
hellspartan <suber10499@gmx.de>
2018-12-09 13:13:00 +0100

    Added Class Pet

--- src/Haupt.java
+++ src/Haupt.java
@@ -3,12 +3,17 @@ public class Haupt {
 
 	public static void main(String[] args) {
 		
-		Person seb = new Person("Sebastian", "Uber");
-		
+		Person seb = new Person("Sebastian", "Uber");		
 		Person han = new Person("Hans", "Uber");
+		
+		Pet cat = new Pet("Lisa", "Katze");
+		Pet dog = new Pet("Wuffi", "Hund");
 		
 		System.out.println("Sohn: " + seb.getFirstName() + " " + seb.getLastName() + ".");
 		System.out.println("Vater: " + han.getFirstName() + " " + han.getLastName() + ".");
+		
+		System.out.println("Haustier Nummer 1: " + cat.getName() + ", Typ: " + cat.getType() + ".");
+		System.out.println("Haustier Nummer 2: " + dog.getName() + ", Typ:  " + dog.getType() + ".");
 
 	}
 
